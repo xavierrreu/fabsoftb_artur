b9e95a33112cedc11f78cf378a1979faa6ba82de
WALTER SILVESTRE COAN <walter.s@univille.br>
2025-03-17 22:05:32 -0300

    revisao java

--- revisao_walter/src/main/java/br/univille/Main.java
+++ revisao_walter/src/main/java/br/univille/Main.java
@@ -4,9 +4,13 @@ public class Main {
     public static void main(String[] args) {
         //System.out.println("Hello world!");
 
-        Cliente zezinho = new Cliente();
+        Cliente zezinho = new Cliente("Zezinho");
         zezinho.setNome("Zezinho da silva Sauro");
         Cliente mariazinha = new Cliente();
         mariazinha.setNome("Mariazinha");
+
+
+        System.out.println(zezinho);
+        System.out.println(mariazinha);
     }
 }
