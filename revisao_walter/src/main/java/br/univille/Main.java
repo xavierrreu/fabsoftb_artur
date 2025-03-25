0b3fd0dd120b1e75c8a95b6cba3074bf0280e5d8
WALTER SILVESTRE COAN <walter.s@univille.br>
2025-03-24 22:11:24 -0300

    aula 24/03

--- revisao_walter/src/main/java/br/univille/Main.java
+++ revisao_walter/src/main/java/br/univille/Main.java
@@ -13,12 +13,29 @@ public class Main {
         joinville.setEstado("Santa Catarina");
         
         Pokemon jigglypuff = new Pokemon("Jigglypuff");
+        Pokemon porygon = new Pokemon("Porygon");
 
         Cliente zezinho = new Cliente("Zezinho");
         zezinho.setNome("Zezinho da silva Sauro");
         zezinho.setCidade(joinville);
 
         zezinho.getListaPokemon().add(jigglypuff);
+        zezinho.getListaPokemon().add(porygon);
+        //zezinho.getListaPokemon().add(123);
+        //zezinho.getListaPokemon().add(2.2);
+        //zezinho.getListaPokemon().add("nao sou um pokemon");
+        //zezinho.getListaPokemon().add(true);
+
+        for(int i=0;i<zezinho.getListaPokemon().size();i++){
+            var umPokemon = zezinho.getListaPokemon().get(i);
+            System.out.println(umPokemon);
+        }
+        for(var umPokemon : zezinho.getListaPokemon()){
+            System.out.println(umPokemon);
+        }
+        //gere um codigo para listar os pokemons utilizando o método stream
+        zezinho.getListaPokemon().stream().forEach(System.out::println);
+
 
         Cliente mariazinha = new Cliente();
         mariazinha.setNome("Mariazinha");
---
+++ revisao_walter/src/main/java/br/univille/view/FrmMain.java
@@ -0,0 +1,19 @@
+package br.univille.view;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class FrmMain extends JFrame {
+
+    public FrmMain() {
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(500,500);
+        setTitle("Meu primeiro Swing");
+        var texto = new JLabel("Meu texto");
+        getContentPane().add(texto);
+        setVisible(true);
+    }
+    public static void main(String[] args) {
+        new FrmMain();
+    }
+}
